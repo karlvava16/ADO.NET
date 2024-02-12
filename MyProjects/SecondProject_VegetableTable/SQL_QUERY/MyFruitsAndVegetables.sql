ff13dbde97044e489783cb768140d0f9449008ad
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2024-02-12 02:23:08 +0200

    done

---
+++ MyProjects/SecondProject_VegetableTable/SQL_QUERY/MyFruitsAndVegetables.sql
@@ -0,0 +1,63 @@
+﻿CREATE DATABASE MyFruitsAndVegetables
+go
+
+
+
+
+use MyFruitsAndVegetables
+CREATE TABLE FruitsAndVegetables (
+    NameOfObject NVARCHAR(50),
+    TypeOfObject NVARCHAR(10),
+    Color NVARCHAR(20),
+    CalorieСontent INT
+);
+go
+INSERT INTO FruitsAndVegetables (NameOfObject, TypeOfObject, Color, CalorieСontent) VALUES 
+    ('Apple', 'fruit', 'red', 52),
+    ('Banana', 'fruit', 'yellow', 89),
+    ('Orange', 'fruit', 'orange', 47),
+    ('Tangerine', 'fruit', 'orange', 38),
+    ('Watermelon', 'fruit', 'green', 30),
+    ('Pineapple', 'fruit', 'yellow', 50),
+    ('Lemon', 'fruit', 'yellow', 29),
+    ('Peach', 'fruit', 'orange', 39),
+    ('Pear', 'fruit', 'green', 57),
+    ('Cherry', 'fruit', 'red', 50),
+    ('Plum', 'fruit', 'purple', 46),
+    ('Kiwi', 'fruit', 'green', 61),
+    ('Carrot', 'vegetable', 'orange', 41),
+    ('Potato', 'vegetable', 'brown', 77),
+    ('Cucumber', 'vegetable', 'green', 16),
+    ('Tomato', 'vegetable', 'red', 18),
+    ('Beetroot', 'vegetable', 'red', 43),
+    ('Cabbage', 'vegetable', 'green', 25),
+    ('Onion', 'vegetable', 'white', 40),
+    ('Garlic', 'vegetable', 'white', 149),
+    ('Pepper', 'vegetable', 'red', 31),
+    ('Eggplant', 'vegetable', 'purple', 24),
+    ('Spinach', 'vegetable', 'green', 23),
+    ('Zucchini', 'vegetable', 'green', 17),
+    ('Artichoke', 'vegetable', 'green', 47),
+    ('Persimmon', 'fruit', 'orange', 81),
+    ('Avocado', 'fruit', 'green', 160),
+    ('Pomegranate', 'fruit', 'red', 83),
+    ('Passionfruit', 'fruit', 'yellow', 68),
+    ('Mango', 'fruit', 'orange', 60),
+    ('Grapefruit', 'fruit', 'pink', 42),
+    ('Lime', 'fruit', 'green', 30),
+    ('Feijoa', 'fruit', 'green', 55),
+    ('Papaya', 'fruit', 'orange', 43),
+    ('Lychee', 'fruit', 'red', 66),
+    ('Date', 'fruit', 'brown', 282),
+    ('Clementine', 'fruit', 'orange', 47),
+    ('Corn', 'vegetable', 'yellow', 86),
+    ('Radish', 'vegetable', 'red', 16),
+    ('Turnip', 'vegetable', 'red', 20),
+    ('Lettuce', 'vegetable', 'green', 15),
+    ('Beans', 'vegetable', 'green', 345),
+    ('Peas', 'vegetable', 'green', 81),
+    ('Pumpkin', 'vegetable', 'orange', 26),
+    ('Zucchini squash', 'vegetable', 'green', 17),
+    ('Red currant', 'fruit', 'red', 44),
+    ('Cherry', 'fruit', 'red', 50),
+    ('Black currant', 'fruit', 'black', 63);
--- MyProjects/SecondProject_VegetableTable/SecondProject_VegetableTable/Program.cs
+++ MyProjects/SecondProject_VegetableTable/SecondProject_VegetableTable/Program.cs
@@ -1,2 +1,140 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SqlClient;
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+uint choiсe = 0;
+
+
+void PrintList()
+{
+    Console.WriteLine("1) Отображение всей информации из таблицы с овощами и фруктами:");
+
+    Console.WriteLine("2) Отображение всех названий овощей и фруктов:");
+
+    Console.WriteLine("3) Отображение всех цветов:");
+
+    Console.WriteLine("4) Показать максимальную калорийность:");
+
+    Console.WriteLine("5) Показать минимальную калорийность:");
+
+    Console.WriteLine("6) Показать среднюю калорийность.");
+
+    Console.WriteLine("7) Показать количество овощей:");
+
+    Console.WriteLine("8) Показать количество фруктов:");
+
+    Console.WriteLine("9) Показать количество овощей и фруктов заданного цвета:");
+
+    Console.WriteLine("10) Показать количество овощей и фруктов каждого цвета:");
+
+    Console.WriteLine("11) Показать овощи и фрукты с калорийностью ниже указанной:");
+
+    Console.WriteLine("12) Показать овощи и фрукты с калорийностью выше указанной:");
+
+    Console.WriteLine("13) Показать овощи и фрукты с калорийностью в указанном диапазоне:");
+
+    Console.WriteLine("14) Показать все овощи и фрукты, у которых цвет желтый или красный:");
+
+}
+
+// choose info
+while (true)
+{
+    Console.Clear();
+    PrintList();
+    Console.Write("\n---------------------------\nEnter choise: ");
+
+    choiсe = Convert.ToUInt32(Console.ReadLine());
+    switch (choiсe)
+    {
+        case 1:
+            DatabaseQuery("SELECT * FROM FruitsAndVegetables");
+            break;
+        case 2:
+            DatabaseQuery("SELECT DISTINCT NameOfObject FROM FruitsAndVegetables");
+            break;
+        case 3:
+            DatabaseQuery("SELECT DISTINCT Color FROM FruitsAndVegetables");
+            break;
+        case 4:
+            DatabaseQuery("SELECT MAX(CalorieСontent) AS MaxCalorieСontent FROM FruitsAndVegetables");
+            break;
+        case 5:
+            DatabaseQuery("SELECT MIN(CalorieСontent) AS MinCalorieСontent FROM FruitsAndVegetables");
+            break;
+        case 6:
+            DatabaseQuery("SELECT AVG(CalorieСontent) AS AvrCalorieСontent FROM FruitsAndVegetables");
+            break;
+        case 7:
+            DatabaseQuery("SELECT COUNT(*) AS AmountOfVegetables FROM FruitsAndVegetables WHERE TypeOfObject = 'vegetable'");
+            break;
+        case 8:
+            DatabaseQuery("SELECT COUNT(*) AS AmountOfFruits FROM FruitsAndVegetables WHERE TypeOfObject = 'fruit'");
+            break;
+        case 9:
+            DatabaseQuery("SELECT Color, COUNT(*) AS Количество FROM FruitsAndVegetables WHERE Color = 'yellow' GROUP BY Color");
+            break;
+        case 10:
+            DatabaseQuery("SELECT Color, COUNT(*) AS Количество FROM FruitsAndVegetables GROUP BY Color");
+            break;
+        case 11:
+            DatabaseQuery("SELECT * FROM FruitsAndVegetables WHERE CalorieСontent < 50");
+            break;
+        case 12:
+            DatabaseQuery("SELECT * FROM FruitsAndVegetables WHERE CalorieСontent > 100");
+            break;
+        case 13:
+            DatabaseQuery("SELECT * FROM FruitsAndVegetables WHERE CalorieСontent BETWEEN 50 AND 100");
+            break;
+        case 14:
+            DatabaseQuery("SELECT * FROM FruitsAndVegetables WHERE Color IN ('yellow', 'red')");
+            break;
+    }
+}
+
+void DatabaseQuery(string str)
+{
+    SqlConnection connect = new SqlConnection(@"Initial Catalog=MyFruitsAndVegetables;Data Source=DESKTOP-UIJQEL8\SQLEXPRESS;Integrated Security=SSPI");
+    SqlCommand command = new SqlCommand();
+    try
+    {
+        connect.Open();
+        command.Connection = connect;
+
+        Console.Clear();
+
+        Console.WriteLine("|Connection is opened|");
+        Thread.Sleep(1000);
+
+        Console.Clear();
+
+
+        command.CommandText = str;
+        SqlDataReader reader = command.ExecuteReader();
+
+        int count = reader.FieldCount;
+
+        while (reader.Read())
+        {
+            string? res = "", temp = "";
+            for (int i = 0; i < count; i++)
+            {
+                temp = reader[i].ToString();
+                res += temp + "  ";
+            }
+            Console.WriteLine(res);
+            res = "";
+        }
+        reader.Close();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    finally
+    {
+        command.Dispose();
+        connect.Close();
+    }
+    Console.ReadKey();
+}
