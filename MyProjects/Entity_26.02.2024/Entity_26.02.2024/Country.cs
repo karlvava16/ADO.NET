f66e48d904615c7e32593d70e3f9c53647a6c0ae
Vladyslav Karlinskyi <vladyslav.karlinskyi@gmail.com>
2024-03-13 10:45:35 +0200

    first entity

---
+++ MyProjects/Entity_26.02.2024/Entity_26.02.2024/Country.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Entity_26._02._2024
+{
+    public class Country
+    {
+        public int CountryId { get; set; }
+        public string Name { get; set; }
+        public string Capital { get; set; }
+        public long Population { get; set; }
+        public double Area { get; set; }
+        public virtual Continent Continent { get; set; } // Making Continent virtual
+
+    }
+
+    public class Continent
+    {
+        public int ContinentId { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public class CountryContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Continent> Continents { get; set; }
+
+        public CountryContext(DbContextOptions<CountryContext> options) : base(options)
+        {
+            if (Database.EnsureCreated())
+            {
+                Continent[] continents = new Continent[]
+            {
+                new Continent() { Name = "Euroasia" },
+                new Continent() { Name = "North America" },
+                new Continent() { Name = "South America" },
+                new Continent() { Name = "Africa" },
+                new Continent() { Name = "Oceania" },
+                new Continent() { Name = "Antarctica" },
+            };
+                Continents.AddRange(continents);
+
+                // Add countries
+                Country[] countries = new Country[]
+                {
+                new Country() { Name = "France", Capital = "Paris", Population = 67886000, Area = 551695, Continent = continents[0] },
+                new Country() { Name = "China", Capital = "Beijing", Population = 1444216107, Area = 9596961, Continent = continents[0] },
+                new Country() { Name = "United States", Capital = "Washington D.C.", Population = 331002651, Area = 9833520, Continent = continents[2] },
+                new Country() { Name = "Brazil", Capital = "Brasilia", Population = 212559417, Area = 8515767, Continent = continents[3] },
+                new Country() { Name = "Nigeria", Capital = "Abuja", Population = 206139587, Area = 923768, Continent = continents[4] },
+                new Country() { Name = "Australia", Capital = "Canberra", Population = 25499884, Area = 7692024, Continent = continents[5] },
+                new Country() { Name = "Germany", Capital = "Berlin", Population = 83783942, Area = 357022, Continent = continents[0] },
+                new Country() { Name = "India", Capital = "New Delhi", Population = 1380004385, Area = 3287263, Continent = continents[0] },
+                };
+                Countries.AddRange(countries);
+
+                SaveChanges();
+            }
+        }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Server=DESKTOP-UIJQEL8\SQLEXPRESS;Database=Countries;Integrated Security=SSPI;TrustServerCertificate=true");
+        }
+    }
+}
+
---
+++ MyProjects/Entity_26.02.2024/Entity_26.02.2024/Program.cs
@@ -0,0 +1,176 @@
+﻿using Entity_26._02._2024;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+
+
+
+try
+{
+    var builder = new ConfigurationBuilder();
+    builder.SetBasePath(Directory.GetCurrentDirectory());
+    builder.AddJsonFile("appsettings.json");
+    var config = builder.Build();
+    string connectionString = config.GetConnectionString("DefaultConnection");
+
+    var optionsBuilder = new DbContextOptionsBuilder<CountryContext>();
+    var options = optionsBuilder.UseSqlServer(connectionString).Options;
+
+    using (CountryContext db = new CountryContext(options))
+    {
+
+        DisplayAllCountries(db);
+        DisplayCountryNames(db);
+        DisplayCapitalNames(db);
+        DisplayEuropeanCountries(db);
+        DisplayCountriesWithAreaGreaterThan(db, 1000000);
+
+        DisplayCountriesWithNameContainsLetters(db, 'a', 'e');
+        DisplayCountriesWithNameStartsWith(db, 'a');
+        DisplayCountriesWithAreaInRange(db, 500000, 1000000);
+        DisplayCountriesWithPopulationGreaterThan(db, 50000000);
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+//Task_4_1
+static void DisplayAllCountries(CountryContext db)
+{
+    Console.WriteLine("All Countries:");
+
+    var allCountriesInfo = db.Countries
+            .Select(c => new
+            {
+                c.CountryId,
+                c.Name,
+                c.Capital,
+                c.Population,
+                c.Area,
+                Continent = c.Continent.Name
+            });
+
+    foreach (var country in allCountriesInfo)
+    {
+        Console.WriteLine(
+            $"{country.Name} - Capital: {country.Capital}, " +
+            $"Population: {country.Population}, " +
+            $"Area: {country.Area}, " +
+            $"Continent: {country.Continent}");
+    }
+    Console.WriteLine();
+}
+
+//Task_4_2
+static void DisplayCountryNames(CountryContext db)
+{
+    Console.WriteLine("Country Names:");
+    var countryNames = db.Countries.Select(c => c.Name);
+    foreach (var name in countryNames)
+    {
+        Console.WriteLine(name);
+    }
+    Console.WriteLine();
+}
+
+//Task_4_3
+static void DisplayCapitalNames(CountryContext db)
+{
+    Console.WriteLine("Capital Names:");
+    var countryCapitals = db.Countries.Select(c => c.Capital);
+    foreach (var capital in countryCapitals)
+    {
+        Console.WriteLine(capital);
+    }
+    Console.WriteLine();
+}
+
+//Task_4_4
+static void DisplayEuropeanCountries(CountryContext db)
+{
+    Console.WriteLine("European Countries:");
+
+    var europeanCountries = db.Countries.Where(c => c.Continent.Name == "Euroasia").Select(c => c.Name);
+
+    foreach (var countryName in europeanCountries)
+    {
+        Console.WriteLine(countryName);
+    }
+    Console.WriteLine();
+}
+
+//Task_4_5
+static void DisplayCountriesWithAreaGreaterThan(CountryContext db, double a)
+{
+    Console.WriteLine($"Countries with area more then {a} :");
+
+    var Countries = db.Countries.Where(c => c.Area > a).Select(c => c.Name);
+
+    foreach (var countryName in Countries)
+    {
+        Console.WriteLine(countryName);
+    }
+    Console.WriteLine();
+}
+
+//Task_5_1
+static void DisplayCountriesWithNameContainsLetters(CountryContext db, params char[] characters)
+{
+    Console.WriteLine($"Countries that contains '{string.Join("', '", characters)}' :");
+
+    var Countries = db.Countries.AsEnumerable().Where(c => characters.All(ch => c.Name.Contains(ch))).Select(c => c.Name);
+    foreach (var countryName in Countries)
+    {
+        Console.WriteLine(countryName);
+    }
+    Console.WriteLine();
+}
+
+
+
+//Task_5_2
+static void DisplayCountriesWithNameStartsWith(CountryContext db, char a)
+{
+    Console.WriteLine($"Countries that starts with letter '{a}' :");
+
+    var Countries = db.Countries.AsEnumerable().Where(c => c.Name.StartsWith(a)).Select(c => c.Name);
+
+    foreach (var countryName in Countries)
+    {
+        Console.WriteLine(countryName);
+    }
+    Console.WriteLine();
+}
+
+
+//Task_5_3
+static void DisplayCountriesWithAreaInRange(CountryContext db, double a, double b)
+{
+    Console.WriteLine($"Countries with area between {a} and {b} :");
+
+    var Countries = db.Countries.Where(c => c.Area > a && c.Area < b).Select(c => c.Name);
+
+    foreach (var countryName in Countries)
+    {
+        Console.WriteLine(countryName);
+    }
+    Console.WriteLine();
+}
+
+
+//Task_5_4
+static void DisplayCountriesWithPopulationGreaterThan(CountryContext db, long a)
+{
+    Console.WriteLine($"Countries with population more then {a} :");
+
+    var Countries = db.Countries.Where(c => c.Population > a).Select(c => c.Name);
+
+    foreach (var countryName in Countries)
+    {
+        Console.WriteLine(countryName);
+    }
+    Console.WriteLine();
+}
